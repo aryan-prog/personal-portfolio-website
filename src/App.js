36a68232b332371e7d9af1d68adcab7bf02d027f
aryan-prog <mandelakalam12364@gmail.com>
2023-09-13 17:35:43 +0530

    projects component completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Banner } from './components/Banner';
 import NavBar from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Skills} from './components/Skills';
+import {Projects} from './components/Projects';
+
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <NavBar/>
       <Banner/>
       <Skills/>
+      <Projects/>
     </div>
   );
 }
