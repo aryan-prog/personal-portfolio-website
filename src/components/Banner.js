1236b38d845735f7f3d4185e90fac51c6386d5c3
aryan-prog <mandelakalam12364@gmail.com>
2023-09-15 15:38:43 +0530

    warnings removed

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,9 +8,10 @@ export const Banner = () => {
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const setIndex = useState(1);
+  let [index, setIndex] = useState(1);
   const toRotate = [ "Frontend Developer", "Software Engineer"];
   const period = 2000;
+  index = 1;
 
   useEffect(() => {
     let ticker = setInterval(() => {
