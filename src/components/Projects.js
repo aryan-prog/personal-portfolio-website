dfc071b9f328e6952ef1baacbc3a5f5a1d00c1bb
aryan-prog <mandelakalam12364@gmail.com>
2023-09-19 12:43:38 +0530

    weather app included

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,7 @@ import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
+import projImg4 from "../assets/img/project-img4.png";
 import expImg1 from "../assets/img/exp-img1.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 
@@ -26,6 +27,12 @@ export const Projects = () => {
       imgUrl: projImg1,
       TechStack: "HTML, CSS, Javascript, ReactJS, Material UI",
       link:"https://github.com/aryan-prog/Google-Keep-Clone"
+    },
+    {
+      title: "Weather App",
+      imgUrl: projImg4,
+      TechStack: "HTML, CSS, Javascript, ReactJS, OpenWeather API",
+      link:"https://weather-app-seth.netlify.app/" 
     }
   ];
   const experience = [
