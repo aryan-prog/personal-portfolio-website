d0fd97a1910dca6f8d513e925e830a3aecc14bbe
aryan-prog <mandelakalam12364@gmail.com>
2023-09-24 14:57:47 +0530

    resume update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -53,7 +53,7 @@ export const Banner = () => {
               <span className="tagline">Welcome to my Portfolio</span>
               <h1>{`Hi! I'm Aryan Seth`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Frontend Developer", "Software Engineer"]'><span className="wrap">{text}</span></span></h1>
               <p>I'm a frontend developer with expertise in C/C++, HTML, CSS, and JavaScript. I've honed my skills in ReactJS, creating interactive and user-friendly websites. Now, I'm exploring Next.js and TypeScript to elevate my capabilities. My portfolio features diverse projects reflecting my commitment to crafting visually stunning and responsive web solutions. Join me on this journey of pushing boundaries and creating exceptional user experiences. Let's build something amazing together!</p>
-              <a href="https://twitter.com/Seth_Sahab3" target='_blank' rel='noopener noreferrer'> Let’s Connect <ArrowRightCircle size={25} /></a>
+              <a href="http://surl.li/lkxpc" target='_blank' rel='noopener noreferrer'> Download Resume <ArrowRightCircle size={25} /></a>
             </div>
           </Col>
           <Col xs={12} md={6} xl={5}>
